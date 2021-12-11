a50d5060189337e6b1282c75acc2c70700892534
Nilo Ferreira <nsmf.gnf@gmail.com>
2021-12-11 17:23:12 -0300

    LoginRouter retorna 400 se o email nao encontrado

---
+++ src/presentation/router/login-router.spec.ts
@@ -0,0 +1,30 @@
+type HttpRequest = {
+  body: {
+    password: string
+  }
+}
+type HttpResponse = {
+  statusCode: number;
+}
+
+class LoginRouter {
+  route(httpRequest: HttpRequest): HttpResponse {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+describe('Rota de login', () => {
+  test('deve retornar 400 se o email não for fornecido', () => {
+    const sut = new LoginRouter();
+    const httpRequest: HttpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
